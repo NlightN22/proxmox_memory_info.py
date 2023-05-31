0c70752511e3440871b2fdaa09f888d85dc2e3d5
NlightN22 <111307382+NlightN22@users.noreply.github.com>
2023-05-31 23:05:31 +0700

    Create proxmox_memory_info.py

---
+++ proxmox_memory_info.py
@@ -0,0 +1,34 @@
+#!/bin/python
+from prettytable import PrettyTable
+
+import subprocess
+
+
+output = subprocess.check_output(['qm', 'list']).decode('utf-8')
+
+
+lines = output.strip().split('\n')
+
+table = PrettyTable()
+table.field_names = ["VM", "MEM (MB)"]
+
+total_memory = 0
+
+for line in lines[1:]:
+    columns = line.split()
+    memory = int(columns[3])
+    name = columns[1]
+    total_memory += memory
+    table.add_row([name, memory])
+
+print(table)
+print("Total VM memory: {} MB".format(total_memory))
+
+host_memory_output = subprocess.check_output(['free', '-m']).decode('utf-8')
+host_memory_lines = host_memory_output.strip().split('\n')
+host_memory_values = host_memory_lines[1].split()[1:]
+host_total_memory = int(host_memory_values[0])
+
+difference = host_total_memory - total_memory
+print("Current Host memory: {} MB".format(host_total_memory))
+print("Difference: {} MB".format(difference))
